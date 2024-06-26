283d80ff2ca8e6ce5ede02d4adb6588015010ac7
Vishal Kumar <vishal.kumar@netsetsoftware.com>
2024-06-26 13:07:02 +0530

    changes

--- src/Common/CalendarComponent.tsx
+++ src/Common/CalendarComponent.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -15,11 +16,10 @@ const CalendarComponent: React.FC<{ onClose: () => void, initialDate: Date | nul
     // Setting start time to current time
     const currentHour = now.getHours();
     const currentMinute = now.getMinutes();
-    const currentSecond = now.getSeconds();
-    setStartTime(`${currentHour}:${currentMinute}:${currentSecond}`);
+    setStartTime(`${currentHour}:${currentMinute}`);
 
     // Setting end time to 23:59:59 of the current date
-    setEndTime('23:59:59');
+    setEndTime('23:59');
 
     setIsDatePickerOpen(true); // Open date picker when component mounts
   }, []);
@@ -33,7 +33,7 @@ const CalendarComponent: React.FC<{ onClose: () => void, initialDate: Date | nul
       const currentHour = now.getHours();
       const currentMinute = now.getMinutes();
       const currentSecond = now.getSeconds();
-      setStartTime(`${currentHour}:${currentMinute}:${currentSecond}`);
+      setStartTime(`${currentHour}:${currentMinute}`);
 
       // Setting end time to 23:59:59 of the selected date
       setEndTime('23:59:59');
@@ -44,18 +44,29 @@ const CalendarComponent: React.FC<{ onClose: () => void, initialDate: Date | nul
 
   return (
     <div className="calendar-modal">
-      <DatePicker 
-        selected={selectedDate} 
-        onChange={handleDateChange} 
+      <DatePicker
+        selected={selectedDate}
+        onChange={handleDateChange}
         dateFormat="yyyy/MM/dd"
         open={isDatePickerOpen}
         onClickOutside={() => setIsDatePickerOpen(false)} // Close on outside click
       />
       {selectedDate && (
         <div>
-          <p>Selected Date: {selectedDate.toDateString()}</p>
-          <p>Start Time: {startTime}</p>
-          <p>End Time: {endTime}</p>
+        <div className='under-date'>
+          {/* <p className='selected-date-time'>Selected Date: <span>{selectedDate.toDateString()}</span></p> */}
+          <p className='selected-date-time position-relative'>
+            <img src={require("../assets/images/up-icon.svg").default} className="up-icon" alt="icons" />
+            <span>{startTime}</span>
+            <img src={require("../assets/images/up-icon.svg").default} className="down-icon" alt="icons" />
+          </p> -
+          <p className='selected-date-time position-relative'>
+            <img src={require("../assets/images/up-icon.svg").default} className="up-icon" alt="icons" />
+            <span>{endTime}</span>
+            <img src={require("../assets/images/up-icon.svg").default} className="down-icon" alt="icons" />
+          </p>
+        </div>
+        <Button className='apply-btn'>APPLY</Button>
         </div>
       )}
     </div>
